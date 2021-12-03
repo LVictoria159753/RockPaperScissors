b5df97e3c2bf13df3270aa9efdad6da2b95419af
Victoria <Lvictoria159753@gmail.com>
2021-12-02 20:46:51 -0800

    pseudocode

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
-const victory = "You win!"; 
-const tie = " You tied ";
-const defeat = "You were defeated!";
+
 
 function computerPlay () {
 //create an array for the computer to choose from
@@ -8,15 +6,65 @@ let computerPlay = ["Rock", "Paper", "Scissors"];
 //allow the computer to pick a random number between 0-2
 let comppick = Math.floor (Math.random ()* 3);
 
-return answer(comppick);
+
+
+
+// Computer Selection 
+
+if (computerSelection === playerSelection){
+    // display "It's a Tie!"
 }
 
-//Score board. Create two variables to keep track 
-
-let compScore=0;
-let playerScore= 0;
-
-if (comppick == playerpick)
-    
+else if (computerSelection = "Rock" && playerSelection = "Paper" ) {
+    // display "Paper beats Rock."
+    playerScore ++;
+    round++;
+}
 
 
+else if (computerSelection = "Paper" && playerSelection = "Rock" ){
+    // display "Paper beats Rock."
+    computerScore ++;
+    round++;
+}
+
+
+else if (computerSelection = "Scissor" && playerSelection = "Rock" ){
+    // display "Rock beats Scissor."
+    playerScore ++;
+    round++;
+}
+}
+else if (computerSelection = "Rock" && playerSelection = "Scissor" ){
+    // display "Rock beats Scissor"
+    computerScore ++;
+    round++;
+}
+}
+else if (computerSelection = "Paper" && playerSelection = "Scissor" ){
+    // display " Scissor beats Paper.";
+    playerScore ++;
+    round++;
+}
+}
+else (computerSelection = "Scissor" && playerSelection = "Paper" ){
+    // display " Scissor beats Paper.";
+    computerScore ++;
+    round++;
+}
+}
+
+
+let playerScore = 0;
+let computerScore= 0;
+let round = 1;
+
+if (round === 5){
+    if (computerScore - playerScore = 0)
+    //display "It's a Tie!"
+}
+else if (computerScore - playerScore = negative)
+    //display "You win!"
+
+else if (computerScore - playerScore = positive result)
+    //display "Computer Wins. Play Again?"
