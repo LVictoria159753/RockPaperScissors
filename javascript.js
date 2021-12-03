182097c8fd9c045f9b739cda428eb1134caebf1a
Victoria <Lvictoria159753@gmail.com>
2021-12-02 21:39:31 -0800

    changes to js file

--- javascript.js
+++ javascript.js
@@ -3,68 +3,83 @@
 function computerPlay () {
 //create an array for the computer to choose from
 let computerPlay = ["Rock", "Paper", "Scissors"];
+let choice;
+let finalResult = document.createElement('h2');
 //allow the computer to pick a random number between 0-2
-let comppick = Math.floor (Math.random ()* 3);
 
+let num = Math.floor(Math.random()* 3);
 
-
+    if(num===0){
+        choice = 'Rock';
+    } else if(num===1){
+        choice = 'Paper';
+    } else{
+        choice = 'Scissors';
+    }
+    return choice;
+}
 
 // Computer Selection 
 
+let playerScore = 0;
+let computerScore= 0;
+
+function playing (playerSelection, computerSelection){
 if (computerSelection === playerSelection){
-    // display "It's a Tie!"
+   alert ("It's a Tie!");
 }
 
 else if (computerSelection = "Rock" && playerSelection = "Paper" ) {
-    // display "Paper beats Rock."
+    alert ("Paper beats Rock.");
     playerScore ++;
-    round++;
 }
 
 
 else if (computerSelection = "Paper" && playerSelection = "Rock" ){
-    // display "Paper beats Rock."
+    alert ("Paper beats Rock.");
     computerScore ++;
-    round++;
+
 }
 
 
 else if (computerSelection = "Scissor" && playerSelection = "Rock" ){
-    // display "Rock beats Scissor."
+    alert ("Rock beats Scissor.");
     playerScore ++;
-    round++;
+
 }
-}
+
 else if (computerSelection = "Rock" && playerSelection = "Scissor" ){
-    // display "Rock beats Scissor"
+    alert ("Rock beats Scissor");
     computerScore ++;
-    round++;
+
 }
-}
+
 else if (computerSelection = "Paper" && playerSelection = "Scissor" ){
-    // display " Scissor beats Paper.";
+    alert(" Scissor beats Paper.");
     playerScore ++;
-    round++;
+
 }
-}
-else (computerSelection = "Scissor" && playerSelection = "Paper" ){
-    // display " Scissor beats Paper.";
+
+else (computerSelection="Scissor" && playerSelection="Paper"){
+    alert( " Scissor beats Paper.");
     computerScore ++;
-    round++;
+
 }
+
 }
 
 
-let playerScore = 0;
-let computerScore= 0;
-let round = 1;
+let round = 0;
+let outcome = (computerScore - playerScore);
 
 if (round === 5){
-    if (computerScore - playerScore = 0)
-    //display "It's a Tie!"
+    else if (outcome = 0) {
+    alert( "It's a Tie!");
 }
-else if (computerScore - playerScore = negative)
-    //display "You win!"
-
-else if (computerScore - playerScore = positive result)
-    //display "Computer Wins. Play Again?"
+else if (outcome < 0) {
+    alert("You win!");
+}
+else (outcome >1){ 
+    alert( "Computer Wins. Play Again?");
+}
+}
