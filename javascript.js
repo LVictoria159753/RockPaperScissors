66b78f5cb93a84d09074b2b828d0c3ad4482047e
Victoria <Lvictoria159753@gmail.com>
2021-12-02 18:31:19 -0800

    added HTML buttons as well as some javascript

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const victory = "You win!"; 
+const tie = " You tied ";
+const defeat = "You were defeated!";
+
+function computerPlay () {
+//create an array for the computer to choose from
+let computerPlay = ["Rock", "Paper", "Scissors"];
+//allow the computer to pick a random number between 0-2
+let comppick = Math.floor (Math.random ()* 3);
+
+return answer(comppick);
+}
+
+//Score board. Create two variables to keep track 
+
+let compScore=0;
+let playerScore= 0;
+
+if (comppick == playerpick)
+    
+
+
