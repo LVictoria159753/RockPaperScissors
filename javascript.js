c5d8b793636fb48536278238b69b31bb34f4d736
Victoria <Lvictoria159753@gmail.com>
2021-12-26 23:28:27 -0800

    Fixed rounds played functionality

--- javascript.js
+++ javascript.js
@@ -1,90 +1,82 @@
+
 //play again button
 const reloadpg= document.querySelector("#resetbutton");
 reloadpg.addEventListener('click',() => location.reload());
 
-
-const buttonsText = document.querySelectorAll('button').textContent;
-
-//when the buttons are clicked we
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(game(button.id));
-    })
-   
-})
-
-
-//plugs the button clicking back into the gameRound function
- function game(x){
-   gameRound(x, computerPlay());
-   
- }
-
 //Randomizes the Computer RPS Selection
-function computerPlay() {
-    const choice = Math.floor(Math.random()*100)
-        if (choice >= 67) {
-            return "scissors";
-        }
-        else if (choice >= 33 && choice <= 66) {
-            return' rock';
-        }
-        else
-            return 'paper';
-}
 
+function computerPlay() {
+    const OPTIONS = ["rock", "paper", "scissors"];
+    let random = Math.floor(Math.random() * 3);
+    return OPTIONS[random];
+  }
 //Keeps Player Score 
 //Keeps Compares player RPS choice and the Computer RPS choice
-let pScore=0;
-let cpuScore=0;
-let round=0;
 
-function gameRound(playerSelection, computerSelection) {
-   
-   
+
+function playRound(playerSelection) {
+
+
+let computerScore = document.getElementById('computer-score');
+let playerScore = document.getElementById('player-score');
+let roundScore = document.getElementById('roundsPlayed');
+let result = document.getElementById('result');
+
+   computerSelection=computerPlay();
     //Tie game
     if (playerSelection === computerSelection) {
-        console.log('Tie game!');
-        return round++;
+        result.textContent= "Tie game!"
+        roundScore.textContent++;
+        return;
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log( 'You lose! Rock covered by paper!');
-        return (cpuScore++, round++);
+        result.textContent= 'You lose! Rock covered by paper!'
+        computerScore.textContent++;
+        roundScore.textContent++;
+        return; 
     } 
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log( 'You win! Rock breaks scissors!');
-        return(pScore++, round++);
+        result.textContent= 'You win! Rock breaks scissors!';
+        playerScore.textContent++;
+        roundScore.textContent++;
+        return; 
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log( 'You lose! Paper cut by scissors!');
-        return (cpuScore++, round++);
+        result.textContent=  'You lose! Paper cut by scissors!';
+        computerScore.textContent++;
+        roundScore.textContent++;
+        return; 
     } 
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log( 'You win! Paper covers rock!');
-        return(pScore++, round++);
+        result.textContent=  'You win! Paper covers rock!';
+        playerScore.textContent++; 
+        roundScore.textContent++;
+        return; 
     } 
 
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log( 'You win! Scissors cut the paper!');
-        return(pScore++, round++);
+        result.textContent= 'You win! Scissors cut the paper!';
+        playerScore.textContent++; 
+        roundScore.textContent++;
+        return; 
     } 
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log( 'You lose! Scissors crushed by rock!');
-        return (cpuScore++, round++);
+        result.textContent= 'You lose! Scissors crushed by rock!';
+        computerScore.textContent++;
+        roundScore.textContent++;
+        return;
     } 
 }
 
+/*
 //player Score and computer Score
 document.getElementById("computer-score").innerHTML= "Computer Score: "+ pScore;
 document.getElementById("player-score").innerHTML= "Player Scored: " + cpuScore;
 document.getElementById("roundsPlayed").innerHTML="Rounds played: " + round++;
-
- 
-document.getElementById("cpuChoice").innerHTML="Computer chose " + computerPlay () ;
-
+*/
+/*
 //Finds who wins after five rounds. 
 function decideWinner(){
     while (round===5){
@@ -99,14 +91,15 @@ function decideWinner(){
         }
      }
     }
+*/
 
-    rock.addEventListener("click",print)
+//changes to print whether User choice is Rock, Paper or Scissor
+rock.addEventListener("click",print)
 
 function print(){
 let x= document.getElementById("rock").textContent;
 document.getElementById("yourChoice").innerHTML=x;
 }
-
 
 paper.addEventListener("click",p1)
 function p1(){
@@ -114,7 +107,6 @@ let x= document.getElementById("paper").textContent;
 
 document.getElementById("yourChoice").innerHTML=x;
 }
-
 
 scissors.addEventListener("click",p2)
 
@@ -124,8 +116,24 @@ let x= document.getElementById("scissors").textContent;
 document.getElementById("yourChoice").innerHTML=x;
 }
 
+/*
 
+const buttonsText = document.querySelectorAll('button').textContent;
+//when the buttons are clicked we
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(game(button.id));
+    })
+   
+})
+*/
 
+document.getElementById('rock').addEventListener('click',
+  () => playRound('rock'));
 
+document.getElementById('paper').addEventListener('click',
+  () => playRound('paper'));
 
-
+document.getElementById('scissors').addEventListener('click',
+  () => playRound('scissors'));
